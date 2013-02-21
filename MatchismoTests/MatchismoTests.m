ec322147bdbd6d8460adcd24fcc6fc0d2cfe3eb8
Marek Štefkovič <acnnair@gmail.com>
2013-02-21 20:11:17 +0100

    More dummy tests

--- MatchismoTests/MatchismoTests.m
+++ MatchismoTests/MatchismoTests.m
@@ -29,4 +29,33 @@
     STAssertEquals(0, 0, @"Zeros are equal");
 }
 
+- (void)testExample1
+{
+    STAssertEquals(0, 0, @"Zeros are equal");
+}
+- (void)testExample2
+{
+    STAssertEquals(0, 1, @"Zeros are equal");
+}
+- (void)testExample3
+{
+    STAssertEquals(0, 0, @"Zeros are equal");
+}
+- (void)testExample4
+{
+    STAssertEquals(0, 0, @"Zeros are equal");
+}
+- (void)testExample5
+{
+    STAssertEquals(0, 0, @"Zeros are equal");
+}
+- (void)testExample6
+{
+    STAssertEquals(0, 1, @"Zeros are equal");
+}
+- (void)testExample7
+{
+    STAssertEquals(0, 0, @"Zeros are equal");
+}
+
 @end
