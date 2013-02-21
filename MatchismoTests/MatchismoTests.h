8817a9f96f1be4ab6f698e291885d0487687736d
Marek Štefkovič <acnnair@gmail.com>
2013-02-21 19:12:32 +0100

    Added tests bundle

---
+++ MatchismoTests/MatchismoTests.h
@@ -0,0 +1,13 @@
+//
+//  MatchismoTests.h
+//  MatchismoTests
+//
+//  Created by Marek Štefkovič on 21.2.2013.
+//  Copyright (c) 2013 Marek Štefkovič. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MatchismoTests : SenTestCase
+
+@end
---
+++ MatchismoTests/MatchismoTests.m
@@ -0,0 +1,32 @@
+//
+//  MatchismoTests.m
+//  MatchismoTests
+//
+//  Created by Marek Štefkovič on 21.2.2013.
+//  Copyright (c) 2013 Marek Štefkovič. All rights reserved.
+//
+
+#import "MatchismoTests.h"
+
+@implementation MatchismoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MatchismoTests");
+}
+
+@end
